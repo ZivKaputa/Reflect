73438a0d228003db78ca6a2f18b51da13f1cfb71
Ziv Kaputa <ziv.kaputa@gmail.com>
2017-10-04 01:07:18 -0500

    Fixed LED control script

--- LED_Control/set_rgb.py
+++ LED_Control/set_rgb.py
@@ -7,7 +7,7 @@ P_GREEN = 22
 P_BLUE = 24
 
 rgb_color = sys.argv[1:]
-set_rgb(rgb_color)
+
 
 def set_rgb(rgb_color):
     red = int(rgb_color[0])
@@ -16,3 +16,6 @@ def set_rgb(rgb_color):
     pi.set_PWM_dutycycle(P_RED, red)
     pi.set_PWM_dutycycle(P_GREEN, green)
     pi.set_PWM_dutycycle(P_BLUE, blue)
+
+
+set_rgb(rgb_color)
