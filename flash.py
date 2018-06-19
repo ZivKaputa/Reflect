84e19e2d31af79250f66396e120f4baa91b8dacd
Ziv Kaputa <ziv.kaputa@gmail.com>
2018-06-18 22:54:42 -0500

    Strobe functionality

--- flash.py
+++ flash.py
@@ -1,9 +1,18 @@
 import pigpio
 import time
+import signal
+import sys
 
 P_RED = 17
 P_GREEN = 22
 P_BLUE = 24
+
+def signal_handler(signal, frame):
+	print("Dying...")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+os.kill(os.getpid(),signal.SIGINT)
 
 def cleanup():
 	pi.set_PWM_dutycycle(P_RED, 0)
@@ -13,7 +22,7 @@ def cleanup():
 pi = pigpio.pi()
 
 while True:
-        
+
 	pi.set_PWM_dutycycle(P_RED, 255)
 	pi.set_PWM_dutycycle(P_GREEN, 255)
 	pi.set_PWM_dutycycle(P_BLUE, 255)
